c5e8295289f7c8abcf19f3030fe4999c55fe0a9b
Aaron Lord <aaron@lorddesign.net>
2017-04-04 17:40:30 -0700

    Restore missing file

---
+++ src/Devlord.Utilities/SettingNotFoundException.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Devlord.Utilities
+{
+    public class SettingNotFoundException : KeyNotFoundException
+    {
+        public SettingNotFoundException(string setting) : base((string) $"{nameof(SettingNotFoundException)}: {setting}")
+        {
+        }
+    }
+}
