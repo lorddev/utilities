3222f86d38f57d27e3c42972d20e88a9ccc0315a
Aaron Lord <aaron@lorddesign.net>
2017-08-05 13:15:47 -0700

    Adapt serialization tests for cross-framework differences

--- test/Devlord.Utilities.Tests/XmlToolsTests.cs
+++ test/Devlord.Utilities.Tests/XmlToolsTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Xunit;
 
 namespace Devlord.Utilities.Tests
 {
@@ -9,18 +10,19 @@ namespace Devlord.Utilities.Tests
         {
             var testObj = new TestData { Id = 45 };
             var stringResults = testObj.ToXmlString();
-#if NETCOREAPP1_1
-            stringResults.ShouldEqual(@"<TestData xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" "
-                                      + @"xmlns:xsd=""http://www.w3.org/2001/XMLSchema""><Id>45</Id>"
-                                + "<LastSeen>0001-01-01T00:00:00</LastSeen></TestData>");
-#endif
 
-#if NET462
-            // In 4.6.2, it looks like they changed the order in which xsd and xsi are output in the string.
-            stringResults.ShouldEqual(@"<TestData xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" "
-                                      + @"xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""><Id>45</Id>"
-                                      + "<LastSeen>0001-01-01T00:00:00</LastSeen></TestData>");
-#endif
+            // Different versions of the framework serialize attributes in a different order.
+            var expected = new[]
+            {
+                @"<TestData xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" "
+                + @"xmlns:xsd=""http://www.w3.org/2001/XMLSchema""><Id>45</Id>"
+                + "<LastSeen>0001-01-01T00:00:00</LastSeen></TestData>",
+                @"<TestData xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" "
+                + @"xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""><Id>45</Id>"
+                + "<LastSeen>0001-01-01T00:00:00</LastSeen></TestData>"
+            };
+            Assert.True(expected.Contains(stringResults), "Format of serialized string is not expected. "
+                   + $"Expected \r\n{expected[0]}\r\nOr\r\n{expected[1]}\r\nBut got:\r\n{stringResults}");
         }
     }
 }
